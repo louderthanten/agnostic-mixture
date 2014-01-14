e3e0e10be5daf25614939a82008fc9fd8c8c1d4b
Travis Gertz <trav@louderthan10.com>
2014-01-14 11:46:05 -0800

    Switch to lib sass for sass compilation

--- static/vendor/vendor.mix.js
+++ static/vendor/vendor.mix.js
@@ -1,7 +1,3 @@
-// Include your plugin scripts here to make a nice, single, minified plugin file
-
-// e.g.) @import 'name.of.plugin.file.js';
-
 @import 'jquery/jquery-ui-1.10.3.custom.js';
 @import 'jquery/jquery.fitvids.js';
 @import 'swipe/swipe.js';
